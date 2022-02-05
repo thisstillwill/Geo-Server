692a652867fbed85fba4b964ab00e209cd0c3d4c
William Svoboda <wsvoboda@princeton.edu>
2022-02-05 03:45:32 -0500

    Add endpoint for returning points

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import re
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import Response
@@ -15,15 +14,14 @@ redis = aioredis.from_url(REDIS_URL, decode_responses=True)
 @app.post("/points")
 async def add_point(request: Request, status_code=201):
     # Process JSON and create identifier
-    point_json = await request.json()
-    point_body = point_json["point"]
-    point_body["id"] = ulid.new().str
+    point = await request.json()
+    point["id"] = ulid.new().str
 
     # Store point as hash using id as the key
-    await redis.hset(point_body["id"], mapping=point_body)
+    await redis.hset(point["id"], mapping=point)
 
     # Register point as geospatial item to Redis using id as name
-    await redis.geoadd("points", point_body["longitude"], point_body["latitude"], point_body["id"])
+    await redis.geoadd("points", point["longitude"], point["latitude"], point["id"])
 
 # Return a list of points within a radius of the given location
 @app.get("/points")
@@ -31,12 +29,13 @@ async def get_points(latitude: float, longitude: float, status_code=200):
     # Find which points are within the search radius
     point_keys = await redis.georadius("points", longitude, latitude, SEARCH_RADIUS, unit="m")
 
-    # Create a dictionary of points to return
-    points = {}
+    # Create an array of points to return
+    points = []
 
     # Add  each point's corresponding information to the dictionary
     for point_key in point_keys:
-        point_body = await redis.hgetall(point_key)
-        point = {"point": point_body}
-        points.update(point)
+        point = await redis.hgetall(point_key)
+        point["latitude"] = float(point["latitude"])
+        point["longitude"] = float(point["longitude"])
+        points.append(point)
     return points

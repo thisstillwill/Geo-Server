1adfc596e15fafdc6eef2c18ae526a6c4015450d
William Svoboda <wsvoboda@princeton.edu>
2022-02-10 23:10:14 -0500

    Implement point expiration. Points only persist for 24 hours and are subsequently removed when nearby points are fetched

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from starlette.requests import Request
 from starlette.responses import Response
 import aioredis
 import ulid
+from datetime import datetime, timedelta
 
 REDIS_URL: str = "redis://redis:6379"
-SEARCH_RADIUS: int = 400
+EXPIRE_TIME_HOURS: int = 24
+SEARCH_RADIUS_METERS: int = 1600
 
 app = FastAPI()
 redis = aioredis.from_url(REDIS_URL, decode_responses=True)
@@ -20,6 +22,10 @@ async def add_point(request: Request, status_code=201):
     # Store point as hash using id as the key
     await redis.hset(point["id"], mapping=point)
 
+    # Set expire time
+    expire_time = datetime.now() + timedelta(hours=EXPIRE_TIME_HOURS)
+    await redis.expireat(point["id"], expire_time)
+
     # Register point as geospatial item to Redis using id as name
     await redis.geoadd("points", point["longitude"], point["latitude"], point["id"])
 
@@ -27,15 +33,21 @@ async def add_point(request: Request, status_code=201):
 @app.get("/points")
 async def get_points(latitude: float, longitude: float, status_code=200):
     # Find which points are within the search radius
-    point_keys = await redis.georadius("points", longitude, latitude, SEARCH_RADIUS, unit="m")
+    point_keys = await redis.georadius("points", longitude, latitude, SEARCH_RADIUS_METERS, unit="m")
 
     # Create an array of points to return
     points = []
 
     # Add  each point's corresponding information to the dictionary
     for point_key in point_keys:
-        point = await redis.hgetall(point_key)
-        point["latitude"] = float(point["latitude"])
-        point["longitude"] = float(point["longitude"])
-        points.append(point)
+
+        # Remove the key from the sorted set if it has already expired
+        if not await redis.exists(point_key):
+            print("This key has expired! Deleting from sorted set...")
+            await redis.zrem("points", point_key)
+        else:
+            point = await redis.hgetall(point_key)
+            point["latitude"] = float(point["latitude"])
+            point["longitude"] = float(point["longitude"])
+            points.append(point)
     return points

a2ef64c4d2bef86d4bd1578fe0e9fd61bba42532
William Svoboda <wsvoboda@princeton.edu>
2022-03-31 23:03:02 -0400

    Improve identity token verification logic

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, FastAPI
 from starlette.requests import Request
 from starlette.responses import Response
 import aioredis
@@ -13,53 +12,67 @@ import httpx
 REDIS_URL: str = "redis://redis:6379"
 EXPIRE_TIME_HOURS: int = 24
 
-APPLE_PUBLIC_KEY_URL = "https://appleid.apple.com/auth/keys"
-APPLE_PUBLIC_KEY = None
+APPLE_PUBLIC_KEYS_URL = "https://appleid.apple.com/auth/keys"
+APPLE_PUBLIC_KEYS = None
 APPLE_KEY_CACHE_EXP = 60 * 60 * 24
 APPLE_LAST_KEY_FETCH = 0
 
 APPLE_APP_ID = "com.williamsvoboda.Geo"
+APPLE_ISSUER = "https://appleid.apple.com"
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+    )
 
 app = FastAPI()
 redis = aioredis.from_url(REDIS_URL, decode_responses=True)
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Find the matching Apple public key given its kid
 async def _fetch_apple_public_key(kid: str):
     # Check to see if the public key is unset or is stale before returning
     global APPLE_LAST_KEY_FETCH
-    global APPLE_PUBLIC_KEY
+    global APPLE_PUBLIC_KEYS
 
-    if (APPLE_LAST_KEY_FETCH + APPLE_KEY_CACHE_EXP) < int(datetime.timestamp(datetime.now())) or APPLE_PUBLIC_KEY is None:
+    if (APPLE_LAST_KEY_FETCH + APPLE_KEY_CACHE_EXP) < int(datetime.timestamp(datetime.now())) or APPLE_PUBLIC_KEYS is None:
         client = httpx.AsyncClient()
-        response = await client.get(APPLE_PUBLIC_KEY_URL)
-        keys = response.json()["keys"]
-        key = [key for key in keys if key["kid"] == kid][0]
-        APPLE_PUBLIC_KEY = RSAAlgorithm.from_jwk(json.dumps(key))
-        APPLE_LAST_KEY_FETCH = int(datetime.timestamp(datetime.now()))
-    return APPLE_PUBLIC_KEY
+        response = await client.get(APPLE_PUBLIC_KEYS_URL)
+        APPLE_PUBLIC_KEYS = response.json()["keys"]
+    
+    key_body = [key for key in APPLE_PUBLIC_KEYS if key["kid"] == kid][0]
+    public_key = RSAAlgorithm.from_jwk(json.dumps(key_body))
+    APPLE_LAST_KEY_FETCH = int(datetime.timestamp(datetime.now()))
+
+    return public_key
 
 # Verify a user's token from Sign in with Apple
-async def verify_token(request: Request):
+async def verify_identity_token(request: Request) -> bool:
     identity_token = request.headers["Authorization"]
     token_header = jwt.get_unverified_header(request.headers["Authorization"])
     kid = token_header["kid"]
     public_key = await _fetch_apple_public_key(kid)
     try:
-        jwt.decode(identity_token, public_key, audience=APPLE_APP_ID, algorithms=["RS256"])
-        print("Verified!")
+        jwt.decode(identity_token, public_key, issuer=APPLE_ISSUER, audience=APPLE_APP_ID, algorithms=["RS256"])
+        print("Verified identity token!")
+        return True
     except jwt.exceptions.ExpiredSignatureError as e:
-        raise Exception("That token has expired")
+        print("Identity token has expired!")
+        return False
     except jwt.exceptions.InvalidAudienceError as e:
-        raise Exception("That token's audience did not match")
+        print("Identity token's audience did not match!")
+        return False
     except Exception as e:
         print(e)
-        raise Exception("An unexpected error occurred")
+        return False
 
 # Register a new user
-@app.post("/auth")
-async def sign_up(request: Request):
-   await verify_token(request)
+@app.post("/users")
+async def sign_up(request: Request, verified_identity_token = Depends(verify_identity_token)):
+    if not verified_identity_token:
+         raise credentials_exception
+    user = await request.json()
+    print(user)
 
 # Add a new point from a client
 @app.post("/points")
